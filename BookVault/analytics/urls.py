5a158cb593248650f4f629f75b45574b7bf82fdf
rekha <rekhaelizabethmathew21@gmail.com>
2024-11-30 21:24:57 +0530

    Email Notification

--- BookVault/analytics/urls.py
+++ BookVault/analytics/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('view_subscription/', views.view_subscription, name='view_subscription'),
     path('digital-books/', views.search_digital_books, name='search_digital_books'),
+    path('send_notification/', views.send_notification, name='send_notification'),
     
 ]
--- BookVault/analytics/views.py
+++ BookVault/analytics/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Count
-from usermanagement.models import Subscription,MemberSubscriptionLog
+from usermanagement.models import Subscription,MemberSubscriptionLog,User
 
 # Create your views here.
 def view_subscription(request):
@@ -29,3 +29,52 @@ def search_digital_books(request):
         digital_books = fetch_google_books_metadata(query)
     
     return render(request, 'client/digital_books.html', {'books': digital_books})
+
+
+from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.conf import settings  # Import your User model
+
+def send_email_notification_to_users(subject, message):
+    """
+    Sends an email notification to all users in the User table using BCC.
+    
+    Parameters:
+    - subject: The subject of the email.
+    - message: The content of the email body.
+    
+    Returns:
+    - str: A success message indicating the emails were sent.
+    """
+    # Retrieve all email addresses from the User table
+    users = User.objects.all()
+    email_addresses = users.values_list('email', flat=True)  # Get a list of email addresses
+
+    # Create the email message
+    email = EmailMessage(
+        subject,              # Subject of the email
+        message,              # Body of the email
+        settings.EMAIL_HOST_USER,  # From email (configured in settings.py)
+        [settings.EMAIL_HOST_USER],  # To field (can be empty or just sender email)
+        bcc=email_addresses,  # Add all users to the BCC field
+    )
+
+    # Send the email
+    email.send(fail_silently=False)
+    
+    return "Emails sent to users successfully."
+
+def send_notification(request):
+    if request.method == 'POST':
+        # Retrieve subject and message from the POST request
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        
+        # Call the function to send email
+        result = send_email_notification_to_users(subject, message)
+        
+        # Pass the result to the template for feedback
+        return render(request, 'admindashboard/send_notification.html', {'result': result})
+    
+    # If not POST, just render the form
+    return render(request, 'admindashboard/send_notification.html')
